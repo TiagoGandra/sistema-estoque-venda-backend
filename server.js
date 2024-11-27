13afbc01f9628e7b8bbfb6e2846824c838ce5398
tiago.gandra <tiagogandrati@gmail.com>
2024-11-27 16:58:06 -0300

    Conectando ao mongodb atlas

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
+import 'dotenv/config';
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+
 
 const app = express();
 app.listen(3000, () => {
-  console.log("Servidor escutando...")
+  console.log("Servidor escutando...");
 });
 
-const produtos = [
-  {
-    id: 1,
-    descricao: "Perfume fofo",
-    quantidade: "5",
-    preco: "40",
-    codigo: 123
-  },
-  {
-    id: 2,
-    descricao: "Perfume fofo 2",
-    quantidade: "3",
-    preco: "45",
-    codigo: 124
-  }
-]
+
 //select all
+async function getTodosProdutos(){
+  const db = conexao.db("estoque-venda");
+  const colecao = db.collection("produtos");
+  return colecao.find().toArray();
+}
 app.get('/produtos', async(req, res) => {
+  const produtos = await getTodosProdutos();
   res.status(200).json(produtos);
 });
 
-//select por código
-function buscarProdutoPorCod(codigo){
-  return produtos.findIndex((prod) => {
-    return prod.codigo === Number(codigo);
-  });
-}
+// //select por código
+// function buscarProdutoPorCod(codigo){
+//   return produtos.findIndex((prod) => {
+//     return prod.codigo === Number(codigo);
+//   });
+// }
 
-app.get('/produtos/:codigo', async(req, res) => {
-  const index = buscarProdutoPorCod(req.params.codigo);
-  res.status(200).json(produtos[index]);
-});
+// app.get('/produtos/:codigo', async(req, res) => {
+//   const index = buscarProdutoPorCod(req.params.codigo);
+//   res.status(200).json(produtos[index]);
+// });
 
