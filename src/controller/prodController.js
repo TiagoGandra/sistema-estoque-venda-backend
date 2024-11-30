536bb2f40d70cc36a3297c457d43ff8f7029f4b0
tiago.gandra <tiagogandrati@gmail.com>
2024-11-29 22:21:49 -0300

    Correção controller

--- src/controller/prodController.js
+++ src/controller/prodController.js
@@ -1,6 +1,18 @@
-import { getTodosProdutos } from "../models/prodModel.js"
+import { getTodosProdutos, cadastrarProduto } from "../models/prodModel.js"; // Importa a função para obter todos os produtos do banco de dados
 
-export async function listarProdutos(req, res) {
-  const prods = await getTodosProdutos();
-  res.status(200).json(prods)
-}
+export async function listarProdutos(req, res) { // Função assíncrona para listar produtos
+  const prods = await getTodosProdutos(); // Obtém os produtos do banco de dados
+  res.status(200).json(prods); // Retorna os produtos em formato JSON com status 200 (sucesso)
+};
+
+export async function registarProduto(req, res){
+  const novoProd = req.body;
+
+  try{
+    const prodCadastrado = await  cadastrarProduto(novoProd);
+    res.status(200).json(prodCadastrado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(400).json({"Erro": "Falha na requisição ao cadastrar produto!"});
+  }
+};
